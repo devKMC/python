3ae5a8b2849b4beefa3f57096331d6aa18297339
devKMC <allcasa@naver.com>
2024-03-05 15:51:54 +0900

    2024-03-05 3:51

--- crawling/crawler.py
+++ crawling/crawler.py
@@ -30,14 +30,34 @@ URL = 'https://naver.com'
 
 response = requests.get(URL)  
 
-print(response)
-print(response.status_code)
+
 
 if response.status_code == 200:
     html = response.text
-    soup = BeautifulSoup(html, 'html.perser')
-    print(soup)
+    soup = BeautifulSoup(html, 'html.parser')
+    a_list = soup.find_all('a')
+    print(a_list)
 else:
     print(response.status_code)
 
 
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+time.sleep(3)
+
+driver.get(URL)
+time.sleep(3)
+
+search_input = driver.find_element(By.ID, 'query')
+search_input.send_keys('제네시스')
+time.sleep(1)
+search_input.send_keys(Keys.ENTER)
+time.sleep(3)
+news_button = driver.find_element(By.CSS_SELECTOR, '#lnb > div.lnb_group > div > div.lnb_nav_area._nav_area_root > div > div.api_flicking_wrap._conveyer_root > div:nth-child(8) > a')
+ActionChains(driver).click(news_button).perform()
+time.sleep(3)
